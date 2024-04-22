195caba8662fbcefab04d3e8eaa5b8377f3a60c8
Pedromonvel94 <el_petercrauch2@hotmail.com>
2024-04-22 18:21:47 -0500

    bug fix

--- script.js
+++ script.js
@@ -8,74 +8,43 @@ open.addEventListener("click", () => {
     menu.classList.remove("ssm:max-lg:opacity-0");
     open.classList.add("ssm:max-lg:opacity-0");
     open.classList.add("ssm:max-lg:invisible");
-})
+});
 
 close.addEventListener("click", () => {
     menu.classList.add("ssm:max-lg:opacity-0");
     menu.classList.add("ssm:max-lg:invisible");
     open.classList.remove("ssm:max-lg:invisible");
     open.classList.remove("ssm:max-lg:opacity-0");
-})
+});
 
-function activarEfecto3D() {
-    const cardsTech = document.querySelectorAll('.technologies');
+cardsTech.forEach(card => {
+    card.addEventListener('mousemove', (evt) => {
+        const {layerX, layerY} = evt;
 
-    cardsTech.forEach(cardTech => {
-        const height = cardTech.clientHeight;
-        const width = cardTech.clientWidth;
+        const height = card.clientHeight;
+        const width = card.clientWidth;
 
-        function mouseMoveHandler(evt) {
-            const {layerX, layerY} = evt
+        const yRotation = (
+            (layerX - width / 2) / width) * 10;
+        
+        const xRotation = (
+            (layerY - height / 2) / height) * 10;
 
-            const yRotation = (
-                (layerX - width / 2) / width
-            ) * 5
-
-            const xRotation = (
-                (layerY - height / 2) / height
-            ) * 5
-
-            const string = `
+        const string = `
             perspective(500px)
-            scale(1.008)
+            scale(1.03)
             rotateX(${xRotation}deg)
-            rotateY(${yRotation}deg)`
+            rotateY(${yRotation}deg)
+            `
+        
+        card.style.transform = string;
+    });
 
-            cardTech.style.transform = string
-        }
-
-        function mouseOutHandler() {
-            cardTech.style.transform = `
+    card.addEventListener('mouseout', () => {
+        card.style.transform = `
             perspective(500px)
             scale(1)
             rotateX(0)
-            rotateY(0)`
-        }
-
-        cardTech.addEventListener('mousemove', mouseMoveHandler);
-        cardTech.addEventListener('mouseout', mouseOutHandler);
+            rotateY(0)`;
     });
-}
-
-function desactivarEfecto3D() {
-    const cardsTech = document.querySelectorAll('.technologies');
-    cardsTech.forEach(cardTech => {
-        cardTech.style.transform = 'none';
-        cardTech.removeEventListener('mousemove', mouseMoveHandler);
-        cardTech.removeEventListener('mouseout', mouseOutHandler);
-    });
-}
-
-window.addEventListener('load', () => {
-    if (window.innerWidth > 1024) {
-        activarEfecto3D();
-    }
-});
-
-window.addEventListener('resize', () => {
-    if (window.innerWidth > 1024) {
-        activarEfecto3D();
-    } else {
-        desactivarEfecto3D();
-    }
 });
